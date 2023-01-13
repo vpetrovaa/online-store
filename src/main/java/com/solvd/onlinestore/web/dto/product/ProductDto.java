ecd10ab724d2d629ecf62224c6ae1fd787739e8a
vpetrovaa <vpetrova.laba@solvd.com>
2023-01-13 14:06:51 +0300

    Product dto changed

--- src/main/java/com/solvd/onlinestore/web/dto/product/ProductDto.java
+++ src/main/java/com/solvd/onlinestore/web/dto/product/ProductDto.java
@@ -6,12 +6,14 @@ import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class ProductDto {
 
     private Long id;
